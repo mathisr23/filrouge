97dce317301fdd64e1052aa04bd883982af1ebbf
Rivière Mathis <mathis.riviere@edu.devinci.fr>
2020-12-20 20:32:17 +0100

    :construction: added CSS to polls

--- creation.php
+++ creation.php
@@ -15,6 +15,7 @@ if(empty($_SESSION['membre'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/creation.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <title>Création d'un sondage</title>
@@ -53,10 +54,11 @@ if(empty($_SESSION['membre'])) {
         </nav>
     </header>
 
-<h1>Creez votre propre sondage !</h1>
 
+    <h1>Créez votre propre sondage !</h1>
 
 <div class="box">
+
   <div class="form">
   
     <form method="POST" action="includes/config.inc.php" class="formulaire">
